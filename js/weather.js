171b7b6fbb3ec28b14f5c8e113b51b3a53539d6a
Peter <pwaine@aquis.eu>
2015-10-25 21:42:26 +0000

    weather module

---
+++ js/weather.js
@@ -0,0 +1,68 @@
+var Weather  = (function() {
+
+   var Weather = function (callback, units) {
+
+
+      //twitter private module
+      var weather = {
+
+         data: undefined,
+
+         search: {
+            url: "http://api.openweathermap.org/data/2.5/forecast?mode=json&appid=bd82977b86bf27fb59a04b61b657fb6f&callback=" + callback
+         },
+
+         setData: function (data) {
+            this.data = data;
+         },
+
+         go: function (config) {
+
+            var scriptBlock, search, docHeader, i, query, scope, args;
+
+            if (config && config.city && config.country) {
+
+               query = [];
+
+               query = app.utils.urlencode(config.city) + ',' +
+               app.utils.urlencode(config.country);
+
+               scriptBlock = document.createElement('script');
+               scriptBlock.language = 'javascript';
+               scriptBlock.type = 'text/javascript';
+               search = this.search;
+
+               scriptBlock.src = search.url +
+               '&q=' + query +
+               '&units=' + app.utils.urlencode(config.units || 'metric')
+               '&rand=' + Math.floor(Math.random() * 10000000);
+
+               docHeader = document.getElementsByTagName('head')[0];
+               docHeader.appendChild(scriptBlock);
+
+               scope = this;
+
+               if (parseInt(this.search.timeOut, 10) > 0) {
+                  setTimeout(function () {
+                     scope.go.apply(scope, args);
+                  }, parseInt(this.search.timeOut, 10));
+
+               }
+
+            }
+            else {
+               throw new Error("config : {city:, country: } required");
+
+            }
+
+         }
+
+      };
+
+      //expose tweet search to the public
+      return weather;
+   }
+
+   return Weather
+
+})()
