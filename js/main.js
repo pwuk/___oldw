ccaaa7f58fd7e44ece715569a48b4927a582f964
Peter <pwaine@aquis.eu>
2015-10-25 01:17:32 +0100

    layout changes

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ app.Weather = function (callback) {
       data: undefined,
 
       search: {
-         url: "http://api.openweathermap.org/data/2.5/forecast?mode=json&appid=bd82977b86bf27fb59a04b61b657fb6f&callback="+callback
+         url: "http://api.openweathermap.org/data/2.5/forecast?units=metric&mode=json&appid=bd82977b86bf27fb59a04b61b657fb6f&callback="+callback
       },
 
       setData : function(data) {
@@ -121,38 +121,139 @@ weather.callback = function(data) {
 
 weather.render = function() {
 
-   var i, target, el, refLI;
+   var i, target, el, refLI, curDate='', dayData = [];
 
-   target = document.getElementById("output");
+   target = document.getElementById("weather");
    refLI = document.getElementById("insertRef");
 
-   i = this.data.list.length-1;
-   for(; i>0; i--) {
+   curDate = this.data.list[0].dt_txt.slice(0,10);
 
-      el = document.createElement("li");
-      el.className = "tweet";
-      el.innerHTML= this.template(this.data.list[i]);
+   for(i = 0; i<=this.data.list.length-1; i++) {
 
-      //insert or append
-      target.insertBefore(el, target.children[0] || null);
-
+      if(curDate === this.data.list[i].dt_txt.slice(0,10)) {
+         dayData.push(this.data.list[i]);
+      }
+      else {
+         addDay.call(this);
+         dayData.push(this.data.list[i]);
+      }
 
    }
+   addDay.call(this);
+
+
+   function addDay() {
+      el = document.createElement("li");
+      el.className = "li-container";
+      el.innerHTML = this.template(dayData);
+      target.insertBefore(el, null); // target.children[0] || null);
+
+      dayData = [];
+      curDate = (this.data.list[i]) ? this.data.list[i].dt_txt.slice(0,10) : '';
+
+   }
+
 
 };
 
 
-weather.template = function(obj) {
+weather.template = function(dayData) {
 
    var template = [];
+   var week = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
    template.push(
-      "<div>",
-      obj.dt_txt,
-      "</div>"
+      '<div class="day">',
+      '<div class="date">',
+      getDate(dayData[0]),
+      getDaySummary(dayData),
+      '</div>',
+      '<div class="date-data">',
+      getTimes(dayData),
+      '</div>',
+      '</div>'
    );
 
    return template.join("");
+
+
+   function getDaySummary(day) {
+
+      var max = 0, min = 9999;
+
+      for (var i=0; i<=day.length-1; i++ ) {
+         min = Math.min(min, day[i].main.temp_min)
+         max = Math.max(max, day[i].main.temp_max)
+      }
+
+      return '<em class="min">Min: ' + min.toString() + '</em>' +
+         '<em>&ndash;</em>'+
+         '<em class="max">Max: ' + max.toString() + '</em>'
+
+   }
+
+
+
+   function getDate(obj) {
+      var dayOfWeek = new Date(obj.dt_txt).getDay();
+
+      return '<p class="day-name">' + week[dayOfWeek] + '</p>';
+
+   }
+
+
+   function getTimes(times) {
+
+      var rtnVal = [];
+
+      for (var i=0; i<=times.length-1; i++) {
+         rtnVal.push(
+            '<div class="data-time">' +
+               '<p class="time">'+times[i].dt_txt.slice(11,16) + '</p>',
+               '<p class="temp">'+getTemp(times[i]) + '</p>',
+               '<p class="weather">'+getWeather(times[i]) + '</p>',
+               '<p class="clouds">'+getClouds(times[i]) + '</p>',
+               '<p class="wind">'+getWind(times[i]) + '</p>',
+               '<p class="rain">'+getRain(times[i]) + '</p>',
+            '</div>'
+         );
+      }
+
+      return rtnVal.join('');
+   }
+
+   function getTemp(obj) {
+      return '<em class="min">' + obj.main.temp_min.toString() + '</em>' +
+            '<em class="current">' + obj.main.temp.toString() + '</em>' +
+         '<em class="max">' + obj.main.temp_max.toString() + '</em>'
+   }
+
+
+   function getWeather(obj) {
+      return '<em class="desc">' + obj.weather[0].description + '</em>';
+   }
+
+   function getClouds(obj) {
+      var value = '';
+      if(obj.clouds.all) {
+         value =  '<em class="clouds">Clouds: ' + obj.clouds.all.toString() + '%</em>'
+      }
+      return value;
+   }
+
+   function getWind(obj) {
+      return value =  '<em class="wind">Wind: ' + obj.wind.speed.toString() + '%</em>'
+   }
+
+   function getRain(obj) {
+      var value='';
+      if(obj.rain['3h']) {
+         value = '<em class="wind">3h Rain: ' + obj.rain['3h'] + '</em>'
+      }
+      return value;
+   }
+
+
 
 };
 
